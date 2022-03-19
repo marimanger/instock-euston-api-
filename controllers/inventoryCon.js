f975882da6bde838360fc35af55b01481b6c5adf
Tanaka Lusengo <79418686+tanaka-lusengo@users.noreply.github.com>
2022-03-19 22:20:57 +0000

    complete back-end routes for delete inventory

--- controllers/inventoryCon.js
+++ controllers/inventoryCon.js
@@ -1,4 +1,5 @@
 const inventoryModel = require("../models/inventoryMod");
+const helper = require("../utils/helpers");
 
 const getAllData = (_req, res) => {
   const inventory = inventoryModel.getAllData();
@@ -7,44 +8,44 @@ const getAllData = (_req, res) => {
 
 const getSomeData = (_req, res) => {
   const inventory = inventoryModel.getSomeData();
-  // console.log(inventory)
-  const someI=[]
-  for(i=0; i< inventory.length; i++){
-    someI[i]={
-      "itemName":inventory[i].itemName,
-      "category":inventory[i].category,
-      "quantity":inventory[i].quantity,
-      "id":inventory[i].id,
-      "warehouseID":inventory[i].warehouseID
-    }
-
+  const someI = [];
+  for (i = 0; i < inventory.length; i++) {
+    someI[i] = {
+      itemName: inventory[i].itemName,
+      category: inventory[i].category,
+      quantity: inventory[i].quantity,
+      id: inventory[i].id,
+      warehouseID: inventory[i].warehouseID,
+    };
   }
   res.status(200).json(someI);
 };
 
-// const getIndividual = (req, res) => {
-//   const currentID = req.params.productId;
-//   const currentProduct = productModel.getIndividual(currentID);
-//   res.status(200).json(currentProduct);
-// };
 const getIndividualInventory = (req, res) => {
   const currentID = req.params.inventoryId;
   const currentInventory = inventoryModel.getIndividualInventory(currentID);
   res.status(200).json(currentInventory);
 };
 
-// const createProduct = (req, res) => {
-//   if (!req.body.name || !req.body.price) {
-//     res.status(400).send("You need to provude the name and price of the item");
-//   }
-//   const newProduct = productModel.createProduct(req.body);
-//   res.status(201).json(newProduct);
-// };
+// delete existing inventory item
+const deleteInventoryItem = (req, res) => {
+  const found = helper.findById(inventoryModel.getAllData(), req.params.id);
+  if (found) {
+    const deletedInventory = inventoryModel.deleteInventoryById(req.params.id);
+    res.status(200).json({
+      msg: "Inventory item has been successfully deleted",
+      Inventory: deletedInventory,
+    });
+  } else {
+    res.status(404).json({
+      errorMessage: `Inventory item ${req.params.name} with ID: ${req.params.id} not found`,
+    });
+  }
+};
 
 module.exports = {
   getAllData,
   getSomeData,
-  // getIndividual,
   getIndividualInventory,
-  // createProduct,
+  deleteInventoryItem,
 };
--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -3,16 +3,13 @@ const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const inventoryController = require("../controllers/inventoryCon");
 
-// examples
+// inventory routes
 router.get("/inventory", inventoryController.getAllData);
-router.get("/inventorypage",inventoryController.getSomeData)
-
+router.get("/inventorypage", inventoryController.getSomeData);
 router.get(
   "/inventory/:inventoryId",
   inventoryController.getIndividualInventory
 );
-// router.get("/products/:productId", productsController.getIndividual);
-
-// router.post("/products/", productsController.createProduct);
+router.delete("/inventory/:id", inventoryController.deleteInventoryItem);
 
 module.exports = router;
