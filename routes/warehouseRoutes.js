b8da57904ff5d0775426d9a08d59865514b5aca2
Tanaka Lusengo <79418686+tanaka-lusengo@users.noreply.github.com>
2022-03-19 22:08:22 +0000

    delete modale back-end complete for warehouse detail page

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -7,5 +7,6 @@ router.get("/warehouse", warehouseController.getAllData);
 router.get("/warehouse/:id", warehouseController.getIndividual);
 router.post("/warehouse/add-new", warehouseController.addNewWarehouse);
 router.put("/warehouse/:id/edit", warehouseController.editWarehouse);
+router.delete("/warehouse/:id", warehouseController.deleteWarehouse);
 
 module.exports = router;
